93d1b6419bff45be3d2b5cbd45e49659b4f9cd8d
angela <angela@technicalgeekery.com>
2019-01-09 16:31:30 -0500

    still tring to create button

--- sketch.js
+++ sketch.js
@@ -8,6 +8,14 @@ function setup (){
                    callback: gotData,
                    simpleSheet: true } );
 
+  function createButton() {
+  var element = document.createElement("button");
+  element.appendChild(document.createTextNode("Click Me!"));
+  var page = document.getElementById("btn");
+  page.appendChild(element);
+
+  console.log(element);
+}
 
 var button = createButton('generate madlib');
 button.mousePressed(generate);
